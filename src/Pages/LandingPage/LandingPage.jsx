9cfb1d6cc1ab14a1e62c4636017a46c5c4fb760f
AlmeenT <tiamiyualameen@ymail.com>
2022-06-19 21:02:04 -0700

    mailchimp stylish

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -3,12 +3,16 @@ import Timer from '../../Components/Timer/Timer'
 import Form from '../../Components/form/Form'
 import Footer from '../../Components/footer/Footer'
 import './LandingPage.css'
+import logo from '../../Assets/images/brand.png'
 
 const LandingPage = () => {
 
   return (
     <div>
-      <h2 className='logo'> RE:CEIVE</h2>
+      <div className='logo'>
+        <img src={logo} alt="" style={{ height:'100px', width:'100px', margin:'10px' }} />
+        <h1 className='first' style={{color:'#f7941d', fontSize:'50px' }} >re</h1><h1 className='sec' style={{fontSize:'50px', color:'#2f3e91', display: 'inline'}} >ceive</h1>
+      </div>
       <p className='Tagline'> seamless,swift and guaranteed </p>
         <div className='Introtext'>
           <h1 className='Text' > Something Awesome is in the works</h1>
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,71 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import './Form.css'
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-import InputField from "./InputField";
 
 
-const Form = ({ status, message, onValidated }) => {
+const Form = () => {
   
-  useEffect(() => {
-    if(status === "success") clearFields();
-  }, [status])
-  
-  const clearFields = () => {
-    setEmail('');
-  }
-
-  const [email, setEmail] = useState('');
-
-
-const handleSubmit = (e) => {  
-  e.preventDefault();
-
-  email &&
-  email.indexOf("@") > -1 &&
-  onValidated({
-      EMAIL: email
-  });
-}
-
   return (
-    <div>
-      <form action="#" id="form" onSubmit={(e) => handleSubmit(e)} >
-        {status === "sending" && (
-          <div className="mc__alert mc__alert--sending">
-            sending...
-          </div>
-        )}
-        {status === "error" && (
-          <div 
-            className="mc__alert mc__alert--error"
-            dangerouslySetInnerHTML={{ __html: message }}
-          />
-        )}
-        {status === "success" && (
-          <div className="mc_success">
-            Received!
-          </div>
-        )}
-        <div className="formContainer">  
-          <div className="email">
-            <InputField
-              onChangeHandler={setEmail}
-              type="email"
-              value={email}
-              placeholder="your@email.com"
-              isRequired
-            />
+    <div className="form">
+          <div className="formHeading">
+            <h2> Stay up to date with our progress</h2>
           </div>
           <div className="button">
-            <InputField
-              label="Notify Me!"
-              type="submit"
-              formValues={[email]}
-            />
-          </div>
-        </div>  
-        
-      </form>
+            <button><a href="https://ymail.us10.list-manage.com/subscribe?u=300386c814c61bec25b5d3eab&id=277948078a"> Notify Me! </a></button>
+          </div>  
     </div>
   );
 };
